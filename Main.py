ba435c2c40a6c9cab643aa893f9327d86e520f17
livelivelive007 <jeremylive096@gmail.com>
2020-11-11 07:34:35 -0600

    Update superficices.

--- Main.py
+++ Main.py
@@ -2,13 +2,13 @@ import pygame
 from pygame.locals import *
 import sys
 import math
+import random
 
 # -----------
 # Constantes
 # -----------
 
-GREEN = (0,255,0)
-BLACK = (0, 0, 0)
+#Colores disponibles
 scale = [(0,0,0),
 (8,8,8),
 (16,16,16),
@@ -43,81 +43,130 @@ scale = [(0,0,0),
 (248,248,248),
 (255,255,255)]
 WHITE = (255, 255, 255)
+
+#Colores de blanco a negro
+scale.reverse()
+
+#Dimensiones de la pantalla
 SCREEN_WIDTH = 1040
 SCREEN_HEIGHT = 880
 
-#Sonar
+#Pantalla
+surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#Superficie de la figura circulo que irá haciendo camino, siendo el rayo
+surface3 = pygame.Surface((0, 0), pygame.SRCALPHA)
+rayo = surface3.get_rect()
+
+#esenario
+pygame.draw.line(surface, WHITE, (5, 5), (5, 875))
+pygame.draw.line(surface, WHITE, (5, 5), (1035, 5))
+pygame.draw.line(surface, WHITE, (5, 875), (1035, 875))
+pygame.draw.line(surface, WHITE, (1035, 5), (1035, 875))
+
+
+#Coordenadainicial del sonar
 sonar = [SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2]
 
-#Rayo
-surface3 = pygame.Surface((0, 0), pygame.SRCALPHA)
-surface3.fill(BLACK)
-rect3 = surface3.get_rect()
+#Rutas de las direcciones al rededor del sonar izq,der,up,down,derSup,derInf,izqSup,izqInf
+posicion = [1,2,3,4,5,6,7,8]
+
+#Direccion del sonar, aleatorio monte carlo #1
+direccionSonar = random.randint(0,8)
+
+#Posicion inicial del rayo
+pos_rayo_x = sonar[0]
+pos_rayo_y = sonar[1]
+
+#Conjunto de puntos que forman un rayo
+puntos_rayo_primario = []
+
+#Matriz de pixeles
+matriz_pixeles = pygame.PixelArray(surface)
+matriz_pixeles[sonar[0]][sonar[1]] = WHITE
+
+#Rayo primario
+#Supongo que el random dio 2, direccion derecha inferior
+for num in range(0, ((SCREEN_WIDTH // 2) // 5) - 16):
+    rayo.x = pos_rayo_x
+    rayo.y = pos_rayo_y
+    punto = pygame.draw.circle(surface, WHITE, (rayo.x, rayo.y), 1)
+    puntos_rayo_primario.append(punto)
+    pos_rayo_x += 5
+    pos_rayo_y += 5
+
 
 # ------------------------------
 # Clases y Funciones utilizadass
 # ------------------------------
+
+#Metodos de obtencion de rayo reflector
+
+#Metodo de formula de reflexion difusa
+
+#Metodo de definir cuantas llamadas recursivas son
+
 
 # ------------------------------
 # Funcion principal del juego
 # ------------------------------
 
 def main():
+    #Inicializo la sincronia con el juego
     pygame.init()
     pygame.display.set_caption("EcoDireccion")
-    surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        
-    #esenario
-    pygame.draw.line(surface, WHITE, (5, 5), (5, 875))
-    pygame.draw.line(surface, WHITE, (5, 5), (1035, 5))
-    pygame.draw.line(surface, WHITE, (5, 875), (1035, 875))
-    pygame.draw.line(surface, WHITE, (1035, 5), (1035, 875))
-
-    #rayos
-    pixAr = pygame.PixelArray(surface)
-
-    #Sonar
-    pixAr[sonar[0]][sonar[1]] = WHITE
     
-    #Rayos
-    #...
-
+    #Ciclo de recursion
     while True:
+
+        #Eventos del juego, teclado no se usa, solo el mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             
+            #Evento de mouse, se mueve circulo
             posx, posy = pygame.mouse.get_pos()
-            rect3.center = (posx, posy)
-            pygame.draw.circle(surface, WHITE, rect3.center, 1)
+            #rayo.center = (posx, posy)
+            #pygame.draw.circle(surface, WHITE, rayo.center, 1)
+
 
             #Colision con sonar
             dist = math.hypot(posx - sonar[0],  posy - sonar[1])
             if dist < (1 + 1):
                 print('Llego al sonar.')
 
-            #Colision con pared
+            #Colision con pared de la izquierda
             for y in range(5,875):
-                dist2 = math.hypot(rect3.x - 5, rect3.y - y)
+                dist2 = math.hypot(rayo.x - 5, rayo.y - y)
                 if dist2 < (1 + 1):
-                    print('Colision con pared.')
+                    print('Colision con pared de la izquierda')
             
+            #Colision con pared de arriba
             for h in range(5, 1035):
-                dist3 = math.hypot(rect3.x - h, rect3.y - 5)
+                dist3 = math.hypot(rayo.x - h, rayo.y - 5)
                 if dist3 < (1 + 1):
-                    print('Colision con pared.')
+                    print('Colision con pared de arriba.')
 
+            #Colision con pared de abajo
             for p in range(5, 1035):
-                dist4 = math.hypot(rect3.x - p, rect3.y - 875)
+                dist4 = math.hypot(rayo.x - p, rayo.y - 875)
                 if dist4 < (1 + 1):
-                    print('Colision con pared.')
+                    print('Colision con pared de abajo.')
+                    print('rayo.x')
+                    print(rayo.x)
+                    print('rayo.y')
+                    print(rayo.y)
 
+            #Colision con pared de la derecha
             for k in range(5, 875):
-                dist5 = math.hypot(rect3.x - 1035, rect3.y - k)
+                dist5 = math.hypot(rayo.x - 1035, rayo.y - k)
                 if dist5 < (1 + 1):
-                    print('Colision con pared.')
+                    print('Colision con pared de la derecha.')
            
+            #distacia
+            #mess='La distancia es de {}'.format( str(int(dist2)))
+
             ##Update
             pygame.display.update()
 
@@ -126,8 +175,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-           #Prueba
-    #print("x:")
-    #print(posx)
-    #print("y:")
-    #print(posy)
+
--- Prueba.py
+++ Prueba.py
@@ -115,3 +115,58 @@ def getScaleColorStructure9():
 #getScaleColorStructure7()
 #getScaleColorStructure8()
 #getScaleColorStructure9()
+
+
+# ------------------------------
+# Comentarios
+# ------------------------------
+
+#if event.type == pygame.MOUSEBUTTONDOWN:
+#if event.type == pygame.MOUSEBUTTONUP:
+#    pass
+
+#pygame.display.update() es lo mismo que pygame.display.flip()
+#screen.fill(BLACK) es lo mismo que screen.blit(surface2,..)
+
+        #surface.fill(BLACK)
+        #surface.blit(surface2, rect2)
+        #surface.blit(surface3, rect3)
+        #surface.blit(surface4, rect4)
+
+surface3 = pygame.Surface((0, 0), pygame.SRCALPHA)
+surface3.fill(BLACK)
+rect3 = surface3.get_rect()
+rect3.center = (200, 200)
+
+##Sonar
+surface4 = pygame.Surface((0, 0), pygame.SRCALPHA)
+surface4.fill(BLACK)
+rect4 = surface4.get_rect()
+rect4.center = (300, 300)
+
+#for circulo in puntos:
+#    surface.fill(BLACK)
+#    surface.blit(surface2, circulo)
+
+pygame.draw.line(surface, WHITE, (400, 210), (400, 400))
+pygame.draw.line(surface, WHITE, (250, 400), (400, 210))
+pygame.draw.line(surface, WHITE, (250, 400), (400, 400))
+
+pygame.draw.line(surface, WHITE, (800, 610), (800, 800))
+pygame.draw.line(surface, WHITE, (650, 800), (800, 610))
+pygame.draw.line(surface, WHITE, (650, 800), (800, 800))
+
+pygame.draw.line(surface, WHITE, (800, 410), (800, 600))
+pygame.draw.line(surface, WHITE, (650, 600), (800, 410))
+pygame.draw.line(surface, WHITE, (650, 600), (800, 600))
+
+
+#Rayo primario
+#Supongo que el random dio 2, direccion derecha inferior
+for num in range(0, ((SCREEN_WIDTH // 2) // 5) - 16):
+    rayo.x = pos_rayo_x
+    rayo.y = pos_rayo_y
+    punto = pygame.draw.circle(surface, WHITE, (rayo.x, rayo.y), 1)
+    puntos_rayo_primario.append(punto)
+    pos_rayo_x += 5
+    pos_rayo_y += 5
